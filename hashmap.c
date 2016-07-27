ecdb90394dd0d248143f52f84406645525f78b47
robinfruitet <rfruitet@gmail.com>
2016-07-27 17:25:27 +0200

    insert + gestion d'arguments + hashmap

--- hashmap.c
+++ hashmap.c
@@ -2,6 +2,90 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void testHash(){
-    printf("TEST HASH\n");
+t_hashmap* hashmap_create(int slots) {
+    t_hashmap* map = malloc(sizeof(t_hashmap));
+	map->slots = slots;
+	map->load_factor = lf;
+	map->grow_factor = gf;
+
+	map->size = 0;
+	map->entries = calloc(slots, sizeof(t_hashmap_entry*));
+
+	return map;
+}
+
+int hashmap_hashcode(char* key){
+	int hash = 0;
+	while(*key)
+        hash += *(key++);
+	return hash;
+}
+
+void hashmap_put(t_hashmap* map, char* key, void* value) {
+    int code = map_generate_hash(key);
+    code = code % map->slots;
+	
+    t_hashmap_entry ** entry = &(map->entries[code]);
+	
+	while(*entry) {
+        if ( 0 == strcmp(key,(*entry)->key)) {
+            (*entry)->value = value;
+            return 0;
+        }
+        entry = &(*entry)->next;
+    }
+    *entry = hashmap_entry_create(key,value);
+    map->size++;
+}
+
+void * hashmap_get(t_hashmap* map, char* key) {
+    int code = map_generate_hash(key);
+    code = code % map->slots;
+    t_hashmap_entry* entry = map->entries[code];
+
+    while(entry) {
+        if (map_generate_hash(key) ==  map_generate_hash(entry->key)) {
+            return entry->value;
+        }
+        entry = entry->next;
+    }
+    return 0;
+}
+
+void hashmap_remove(t_hashmap* map, char* key) {
+    int code = map_generate_hash(key) % map->slots;
+    t_hashmap_entry** entry = &(map->entries[code]);
+    while(*entry) {
+        if (strcmp((*entry)->key, key) == 0) {
+            t_hashmap_entry* toDel = *entry;
+            *entry = (*entry)->next;
+            free(toDel);
+            map->size--;
+            return;
+        }
+        entry = &((*entry)->next);
+    }
+}
+
+void hashmap_delete(t_hashmap* map, char* key) {
+    int code = map_generate_hash(key) % map->slots;
+    t_hashmap_entry** entry = &(map->entries[code]);
+    while(*entry) {
+        if (strcmp((*entry)->key, key) == 0) {
+            t_hashmap_entry* toDel = *entry;
+            *entry = (*entry)->next;
+            free(toDel);
+            map->size--;
+            return;
+        }
+        entry = &((*entry)->next);
+    }
+}
+
+void* hashmap_traverse(t_hashmap* map, char* path) {
+		
+}
+
+void hashmap_put(t_hashmap*map, char* path, void* value){
+	
 }
--- json_parse.c
+++ json_parse.c
@@ -1,15 +1,44 @@
 #include "json_parse.h"
+#include "hashmap.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-void testJson(){
-    printf("TEST JSON\n");
-}
-
 t_hashmap* JSON_parse(char* string) {// Convert a JSON String to a HashMap representation.
+/**    t_hashmap * map = hashmap_create(100);
 
+    while() {
+
+        char* key = NULL;
+        char* value = NULL;
+
+        hashmap_put(map, key, value);
+    }
+
+    return map;*/
 }
 
 char* JSON_stringify(t_hashmap* map) {// Convert a HashMap to a JSON String representation.
+/**    unsigned int i = 0;
+    t_hashmap_entry* tmp;
+    while(i < map->slots) {
+        tmp = map->entry[i];
+        while(tmp != NULL) {
+            printf("%s -> %s\n", tmp->key, tmp->value);
+            tmp = tmp->next;
+        }
+        i++;
+    }*/
+}
 
+FILE* fileNosql(){
+    FILE* file = NULL;
+    file = fopen("test.json", "r+");
+
+    if (file == NULL) {
+        printf("FILE ERROR\n");
+        return 0;
+    } else {
+        printf("File nosql find\n");
+        return file;
+    }
 }
